4a8898633881f71ab429144bb10257ca14fb3bbf
Eugene Borodkin <eaborodkin@gmail.com>
2023-09-05 23:47:55 +0000

    Removed delete blog functionality. Fetch data functionality refactored to the useFetch.js

--- src/Home.js
+++ src/Home.js
@@ -1,40 +1,15 @@
 import {useState, useEffect} from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
-
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id)
-        setBlogs(newBlogs)
-    }
-
-    useEffect(() => {
-        fetch('//localhost:8000/blogs')
-            .then(response => {
-                if (!response.ok) {
-                    throw Error('Something went wrong! Try your attempt later.')
-                }
-                return response.json()
-            })
-            .then(data => {
-                setBlogs(data)
-                setIsPending(false)
-                setError(null)
-            })
-            .catch(error => {
-                setIsPending(false)
-                setError(error.message)
-            })
-    }, []);
+    const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
     return (
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
+            {blogs && <BlogList blogs={blogs} title="All blogs"/>}
         </div>
     )
 }
---
+++ src/useFetch.js
@@ -0,0 +1,30 @@
+import {useEffect, useState} from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw Error('Something went wrong! Try your attempt later.')
+                }
+                return response.json()
+            })
+            .then(data => {
+                setData(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(error => {
+                setIsPending(false)
+                setError(error.message)
+            })
+    }, [url])
+
+    return {data, isPending, error}
+}
+
+export default useFetch
