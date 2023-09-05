76c9ba9635cfcf407dc6abdc910789c23e301c90
Eugene Borodkin <eaborodkin@gmail.com>
2023-09-05 23:06:53 +0000

    Fetch errors handled.

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,8 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id)
@@ -10,15 +12,28 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(response => response.json())
+        fetch('//localhost:8000/blogs')
+            .then(response => {
+                if (!response.ok) {
+                    throw Error('Something went wrong! Try your attempt later.')
+                }
+                return response.json()
+            })
             .then(data => {
                 setBlogs(data)
+                setIsPending(false)
+                setError(null)
+            })
+            .catch(error => {
+                setIsPending(false)
+                setError(error.message)
             })
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>}
         </div>
     )
